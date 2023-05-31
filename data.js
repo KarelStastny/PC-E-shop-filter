34475d6a01d94da54ba5d8f66df1fad2b0f43fd1
Karel Šťastný <karell29@icloud.com>
2023-05-31 14:44:45 +0200

    Vytvořeny data.js a zadána první data

---
+++ data.js
@@ -0,0 +1,34 @@
+const product = [{
+    id: 1,
+    quantity: 20,
+    name: "MacBook Air 13",
+    category: "Pracovní",
+    price: 25000,
+    img: "./images_product/1.jpg",
+    značka:"Macbook",
+    details: {
+        procesor: "Apple M1",
+        os: "mac os",
+        memory: "8GB",
+        dispaly:"lesklý",
+    },
+
+},{
+    id: 2,
+    quantity: 25,
+    name: "MacBook Air 13",
+    category: "Herní",
+    price: 23900,
+    img: "./images_product/2.jpg",
+    značka:"HP",
+    details: {
+        procesor: "Intel Core i5 1235U Alder Lake",
+        os: "Windows 11 Home",
+        memory: "16GB",
+        dispaly:"matný",
+    },
+
+},
+]
+
+// Pracovní, Herní, Ultrabook, 
